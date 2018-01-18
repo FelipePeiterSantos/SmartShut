6e1da366d6f21aedfc20385ec19b369f769b9b07
FelipePeiter <felipe.peiter@gmail.com>
2018-01-18 11:08:58 -0200

    Commit

---
+++ androidManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class androidManager : MonoBehaviour {
+
+    public UnityEngine.UI.InputField inputIP;
+    public UnityEngine.UI.Text textTop;
+    public GameObject connect;
+
+    void Start() {
+        textTop.text = "Connect your device";
+        if(PlayerPrefs.GetString("IPServer") != "") {
+            inputIP.text = PlayerPrefs.GetString("IPServer");
+        }
+    }
+
+    void Update() {
+        if(inputIP.text != "" && !connect.activeSelf) {
+            connect.SetActive(true);
+        }
+        else if(inputIP.text == "" && connect.activeSelf) {
+            connect.SetActive(false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            ExitButton();
+        }
+    }
+
+    public void ConnectButton() {
+        textTop.text = "Connecting...";
+        Network.Connect(inputIP.text,25001);
+    }
+
+    public void ExitButton() {
+        Network.Disconnect();
+        Application.Quit();
+    }
+
+    public void ShutdownButton() {
+
+    }
+
+    void OnConnectedToServer() {
+        textTop.text = "Connected!";
+        PlayerPrefs.SetString("IPServer", inputIP.text);
+    }
+
+    void OnDisconnectedFromServer() {
+        textTop.text = "Disconnected.";
+        inputIP.text = "";
+    }
+
+    void OnFailedToConnect() {
+        textTop.text = "Fail to connect.";
+        inputIP.text = "";
+    }
+}
---
+++ serverManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Diagnostics;
+using System.Collections;
+
+public class serverManager : MonoBehaviour {
+
+    public UnityEngine.UI.Text status;
+
+    void Start() {
+        status.text = "Disconnected";
+        InvokeRepeating("UpdateScript",1f,1f);
+        if(Network.peerType == NetworkPeerType.Disconnected) {
+            Network.InitializeServer(1, 25001,false);
+            status.text = "Activating...";
+        }
+    }
+
+    void UpdateScript() {
+        if(Network.peerType == NetworkPeerType.Server) {
+            if(Network.connections.Length <= 0) {
+                status.text = "Connect your device to '" + Network.player.ipAddress+"' to Shutdown this PC.";
+            }
+            else {
+                status.text = "Shutting down...";
+                Process.Start(Application.streamingAssetsPath + "/shutdown.bat");
+                Application.Quit();
+            }
+        }
+    }
+}
